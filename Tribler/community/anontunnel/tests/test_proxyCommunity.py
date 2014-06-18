51725fa82b94f83cd204b1985190ba8b2a3938f2
Chris <risto@live.nl>
2014-06-18 14:07:06 +0200

    Fix unit test

--- Tribler/community/anontunnel/tests/test_proxyCommunity.py
+++ Tribler/community/anontunnel/tests/test_proxyCommunity.py
@@ -19,7 +19,8 @@ from Tribler.community.anontunnel.payload import (CreateMessage, CreatedMessage,
 from Tribler.community.anontunnel.routing import Circuit
 from Tribler.dispersy.candidate import WalkCandidate, CANDIDATE_ELIGIBLE_DELAY
 from Tribler.dispersy.endpoint import NullEndpoint
-from Tribler.dispersy.util import call_on_reactor_thread
+from Tribler.dispersy.util import call_on_reactor_thread, \
+    blocking_call_on_reactor_thread
 
 
 __author__ = 'Chris'
@@ -34,31 +35,37 @@ class DummyEndpoint(NullEndpoint):
 
 
 class TestProxyCommunity(TestAsServer):
-    @call_on_reactor_thread
+
     def setUp(self):
-        super(TestProxyCommunity, self).setUp()
-        self.__candidate_counter = 0
-        self.dispersy = self.session.lm.dispersy
+        TestAsServer.setUp(self)
 
-        dispersy = self.dispersy
+        @blocking_call_on_reactor_thread
+        def _setUp():
+            self.__candidate_counter = 0
+            self.dispersy = self.session.lm.dispersy
 
-        keypair = dispersy.crypto.generate_key(u"NID_secp160k1")
-        dispersy_member = dispersy.get_member(private_key=dispersy.crypto.key_to_bin(keypair))
+            dispersy = self.dispersy
 
-        settings = ProxySettings()
-        settings.crypto = NoCrypto()
+            keypair = dispersy.crypto.generate_key(u"NID_secp160k1")
+            dispersy_member = dispersy.get_member(private_key=dispersy.crypto.key_to_bin(keypair))
 
-        self.community = dispersy.define_auto_load(ProxyCommunity, dispersy_member, (settings, None), load=True)[0]
-        exit_strategy = exitstrategies.DefaultExitStrategy(self.session.lm.rawserver, self.community)
-        self.community.observers.append(exit_strategy)
+            settings = ProxySettings()
+            settings.crypto = NoCrypto()
 
-        ''' :type : ProxyCommunity '''
+            self.community = dispersy.define_auto_load(ProxyCommunity, dispersy_member, (None, settings, self.session.lm.rawserver), load=True)[0]
+
+            exit_strategy = exitstrategies.DefaultExitStrategy(self.session.lm.rawserver, self.community)
+            self.community.observers.append(exit_strategy)
+
+            ''' :type : ProxyCommunity '''
+
+        _setUp()
 
     def setUpPreSession(self):
-        super(TestProxyCommunity, self).setUpPreSession()
+        TestAsServer.setUpPreSession(self)
         self.config.set_dispersy(True)
 
-    @call_on_reactor_thread
+    @blocking_call_on_reactor_thread
     def __create_walk_candidate(self):
         self.__candidate_counter += 1
         wan_address = ("8.8.8.{0}".format(self.__candidate_counter), self.__candidate_counter)
@@ -72,9 +79,8 @@ class TestProxyCommunity(TestAsServer):
         now = time.time()
         candidate.walk(now - CANDIDATE_ELIGIBLE_DELAY)
         candidate.walk_response(now)
+        self.community.add_candidate(candidate)
         return candidate
-
-
 
     def test_on_create(self):
         create_sender = self.__create_walk_candidate()
@@ -104,7 +110,7 @@ class TestProxyCommunity(TestAsServer):
 
         # Newly created circuit should be stored in circuits dict
         self.assertIsInstance(circuit, Circuit)
-        self.assertEqual(create_sender, circuit.first_hop)
+        self.assertEqual(create_sender.sock_addr, circuit.first_hop)
         self.assertEqual(hops, circuit.goal_hops)
         self.assertIn(circuit.circuit_id, self.community.circuits)
         self.assertEqual(circuit, self.community.circuits[circuit.circuit_id])
@@ -114,7 +120,7 @@ class TestProxyCommunity(TestAsServer):
         args, kwargs = send_message.call_args
         destination, reply_circuit, message_type, created_message = args
         self.assertEqual(circuit.circuit_id, reply_circuit)
-        self.assertEqual(create_sender, destination)
+        self.assertEqual(create_sender.sock_addr, destination)
         self.assertEqual(MESSAGE_CREATE, message_type)
         self.assertIsInstance(created_message, CreateMessage)
 
@@ -122,7 +128,7 @@ class TestProxyCommunity(TestAsServer):
         first_hop = self.__create_walk_candidate()
         circuit = self.community.create_circuit(first_hop, 1)
 
-        self.community.on_created(circuit.circuit_id, first_hop, CreatedMessage([]))
+        self.community.on_created(circuit.circuit_id, first_hop.sock_addr, CreatedMessage([]))
         self.assertEqual(CIRCUIT_STATE_READY, circuit.state)
 
     def test_on_extended(self):
@@ -130,7 +136,7 @@ class TestProxyCommunity(TestAsServer):
         first_hop = self.__create_walk_candidate()
         circuit = self.community.create_circuit(first_hop, 2)
 
-        result = self.community.on_created(circuit.circuit_id, first_hop, CreatedMessage([]))
+        result = self.community.on_created(circuit.circuit_id, first_hop.sock_addr, CreatedMessage([]))
         self.assertFalse(result)
         self.assertEqual(CIRCUIT_STATE_BROKEN, circuit.state)
 
@@ -146,21 +152,21 @@ class TestProxyCommunity(TestAsServer):
 
         self.community.send_message = send_message = Mock()
 
-        result = self.community.on_created(circuit.circuit_id, first_hop, CreatedMessage(candidate_list))
+        result = self.community.on_created(circuit.circuit_id, first_hop.sock_addr, CreatedMessage(candidate_list))
         self.assertTrue(result)
 
         # ProxyCommunity should send an EXTEND message with the hash of second_hop's pub-key
         args, kwargs = send_message.call_args
         circuit_candidate, circuit_id, message_type, message = args
 
-        self.assertEqual(first_hop, circuit_candidate)
+        self.assertEqual(first_hop.sock_addr, circuit_candidate)
         self.assertEqual(circuit.circuit_id, circuit_id)
         self.assertEqual(MESSAGE_EXTEND, message_type)
         self.assertIsInstance(message, ExtendMessage)
         self.assertEqual(message.extend_with, public_bin)
 
         # Upon reception of the ON_EXTENDED the circuit should reach it full 2-hop length and thus be ready for use
-        result = self.community.on_extended(circuit.circuit_id, first_hop, ExtendedMessage("", []))
+        result = self.community.on_extended(circuit.circuit_id, first_hop.sock_addr, ExtendedMessage("", []))
         self.assertTrue(result)
         self.assertEqual(CIRCUIT_STATE_READY, circuit.state)
 
@@ -175,7 +181,7 @@ class TestProxyCommunity(TestAsServer):
     def test_on_data(self):
         first_hop = self.__create_walk_candidate()
         circuit = self.community.create_circuit(first_hop, 1)
-        self.community.on_created(circuit.circuit_id, first_hop, CreatedMessage([]))
+        self.community.on_created(circuit.circuit_id, first_hop.sock_addr, CreatedMessage([]))
 
         payload = "Hello world"
         origin = ("google.com", 80)
@@ -186,15 +192,15 @@ class TestProxyCommunity(TestAsServer):
         self.community.observers.append(observer)
 
         # Its on our own circuit so it should trigger the on_incoming_from_tunnel event
-        self.community.on_data(circuit.circuit_id, first_hop, data_message)
+        self.community.on_data(circuit.circuit_id, first_hop.sock_addr, data_message)
         on_incoming_from_tunnel.assert_called_with(self.community, circuit, origin, payload)
 
         # Not our own circuit so we need to exit it
         destination = ("google.com", 80)
         exit_message = DataMessage(destination, payload, origin=None)
         observer.on_exiting_from_tunnel = on_exiting_from_tunnel = Mock()
-        self.community.on_data(1337, first_hop, exit_message)
-        on_exiting_from_tunnel.assert_called_with(1337, first_hop, destination, payload)
+        self.community.on_data(1337, first_hop.sock_addr, exit_message)
+        on_exiting_from_tunnel.assert_called_with(1337, first_hop.sock_addr, destination, payload)
 
     def test_on_extend(self):
         # We mimick the intermediary hop ( ORIGINATOR - INTERMEDIARY - NODE_TO_EXTEND_ORIGINATORS_CIRCUIT_WITH )
@@ -219,12 +225,12 @@ class TestProxyCommunity(TestAsServer):
         self.assertIsInstance(created_message, CreatedMessage)
         self.assertIn(extend_pub_key, candidate_dict)
 
-        self.community.on_extend(originator_circuit_id, originator, ExtendMessage(extend_pub_key))
+        self.community.on_extend(originator_circuit_id, originator.sock_addr, ExtendMessage(extend_pub_key))
 
         # Check whether we are sending a CREATE to node_to_extend_with
         args, kwargs = send_message.call_args
         create_destination, circuit_id, message_type, message = args
-        self.assertEqual(node_to_extend_with, create_destination)
+        self.assertEqual(node_to_extend_with.sock_addr, create_destination)
         self.assertEqual(MESSAGE_CREATE, message_type)
         self.assertIsInstance(message, CreateMessage)
 
@@ -238,16 +244,9 @@ class TestProxyCommunity(TestAsServer):
     def test_on_pong(self):
         first_hop = self.__create_walk_candidate()
         circuit = self.community.create_circuit(first_hop, 1)
-        self.community.on_created(circuit.circuit_id, first_hop, CreatedMessage({}))
+        self.community.on_created(circuit.circuit_id, first_hop.sock_addr, CreatedMessage({}))
 
-        result = self.community.on_pong(circuit.circuit_id, first_hop, PongMessage())
-        self.assertFalse(result, "Cannot handle a pong when we never sent a PING")
-
-        self.community.create_ping(first_hop, circuit)
-
-        # Check whether the circuit last incoming time is correct after the pong
-        circuit.last_incoming = 0
-        result = self.community.on_pong(circuit.circuit_id, first_hop, PongMessage())
+        result = self.community.on_pong(circuit.circuit_id, first_hop.sock_addr, PongMessage())
         self.assertTrue(result)
 
         self.assertAlmostEqual(circuit.last_incoming, time.time(), delta=0.5)
@@ -257,15 +256,15 @@ class TestProxyCommunity(TestAsServer):
         first_hop = self.__create_walk_candidate()
         self.community.add_candidate(first_hop)
 
-        self.community.on_create(circuit_id, first_hop, CreateMessage())
+        self.community.on_create(circuit_id, first_hop.sock_addr, CreateMessage())
 
         self.community.send_message = send_message = Mock()
-        self.community.on_ping(circuit_id, first_hop, PingMessage())
+        self.community.on_ping(circuit_id, first_hop.sock_addr, PingMessage())
 
         # Check whether we responded with a pong
         args, kwargs = send_message.call_args
 
-        self.assertEqual(first_hop, kwargs['destination'])
+        self.assertEqual(first_hop.sock_addr, kwargs['destination'])
         self.assertEqual(circuit_id, kwargs['circuit_id'])
         self.assertEqual(MESSAGE_PONG, kwargs['message_type'])
         self.assertIsInstance(kwargs['message'], PongMessage)
